791ddcd2099d29192dcece4bca37af3da8dcf1fa
Mr Purple <ash.rawstron@gmail.com>
2019-03-23 08:53:04 -0600

    Add test script for weapon-collision.

---
+++ Assets/TestWeaponCollision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestWeaponCollision : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Collision Entered:: " + collider.tag);
+    }
+}
